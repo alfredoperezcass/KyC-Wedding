07288139d60216d8c879d8befb538b349b87f30b
alfredoperezcass <150087851+alfredoperezcass@users.noreply.github.com>
2024-01-12 21:51:23 -0500

    Adjust paddin top

--- src/app/pages/passes/passes.component.ts
+++ src/app/pages/passes/passes.component.ts
@@ -5,26 +5,28 @@ import { GlobalState } from '@core/global.state'
 @Component({
 	template: `
 		<ng-container *ngIf="guest$ | async as guest">
-			<h1 class="cursive animate__animated animate__fadeInDown">{{ guest.title }}</h1>
-			<strong class="animate__animated animate__fadeInUp animate__delay-1s">
-				{{ 'PASSES.WE_MARRIED' | translate }}
-			</strong>
-			<p class="animate__animated animate__fadeInDown animate__delay-2s">
-			{{ 'PASSES.MESSAGE_TEXT' | translate }}
-			</p>
-			<p class="animate__animated animate__fadeInDown animate__delay-2s">
-			{{ 'PASSES.MESSAGE' | translate }}
-			</p>
-			<span class="animate__animated animate__fadeInUp animate__delay-3s">
-				{{ guest.passes }}
-			</span>
-			<small class="animate__animated animate__fadeInDown animate__delay-4s">
-				{{ 'PASSES.NO_KIDS' | translate }}
-			</small>
-			<div class="buttons-container">
-				<button class="wedding-button">Confirmar Asistencia</button>
-				<button class="wedding-button">Declinar</button>
-				<button class="wedding-button">No estoy Seguro(a)</button>
+			<div class="padding-head">
+				<h1 class="cursive animate__animated animate__fadeInDown">{{ guest.title }}</h1>
+				<strong class="animate__animated animate__fadeInUp animate__delay-1s">
+					{{ 'PASSES.WE_MARRIED' | translate }}
+				</strong>
+				<p class="animate__animated animate__fadeInDown animate__delay-2s">
+				{{ 'PASSES.MESSAGE_TEXT' | translate }}
+				</p>
+				<p class="animate__animated animate__fadeInDown animate__delay-2s">
+				{{ 'PASSES.MESSAGE' | translate }}
+				</p>
+				<span class="animate__animated animate__fadeInUp animate__delay-3s">
+					{{ guest.passes }}
+				</span>
+				<small class="animate__animated animate__fadeInDown animate__delay-4s">
+					{{ 'PASSES.NO_KIDS' | translate }}
+				</small>
+				<div class="buttons-container">
+					<button class="wedding-button">Confirmar Asistencia</button>
+					<button class="wedding-button">Declinar</button>
+					<button class="wedding-button">No estoy Seguro(a)</button>
+				</div>
 			</div>
 		</ng-container>
 	`,
